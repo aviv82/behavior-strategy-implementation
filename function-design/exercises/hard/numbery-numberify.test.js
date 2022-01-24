7eb5d840b5cebf9a6250bdb9b9c6ba79ea900b11
Aviv <89319835+aviv82@users.noreply.github.com>
2022-01-25 00:39:46 +0100

    Update numbery-numberify.test.js

--- function-design/exercises/hard/numbery-numberify.test.js
+++ function-design/exercises/hard/numbery-numberify.test.js
@@ -1,5 +1,6 @@
 // #todo
 
+
 'use strict';
 
 /**
@@ -11,23 +12,92 @@
  * @example
  *  ['1', '2', 'e', '.'] // --> [1, 2]
  */
-const stub = () => {};
+const stub = (string = []) => {
+  return number = [];
+};
 
 /*
-
+strategy:
+1. declare empty array
+2. check if argument is array
+3. check if argument contains non string values
+4. convert array values to numbers
+5. filter NaN values
+6. construct new array
+7. return array 
 */
 
+const firstTry = (arr) => {
+let newArr = [];
+if(Array.isArray(arr)) {
+for (const value of arr) {
+  if (typeof value !== 'string'){
+    throw new TypeError("arrayOfStrings contains non-strings");
+  } else if (Number(value) !== NaN) {
+newArr.push(Number(value));
+console.log(value, newArr);
+  }
+}
+  return newArr;
+} 
+throw new TypeError("arrayOfStrings is not an array");
+};
+
 for (const solution of [
-  secretSolution,
+  // secretSolution,
+  firstTry
   // stub
 ]) {
-  describe(solution.name + ': _', () => {
-    describe('_', () => {
-      it('_', () => {});
+  describe(solution.name + ': converts an array of strings into an array of numbers', () => {
+    describe('parameter is an empty array', () => {
+      it('return an empty array', () => {
+expect(solution([])).toEqual([]);
+      });
     });
-  });
-}
+  /*  
+  describe('parameter is not an array', () => {
+      it('return TypeError message', () => {
+expect(solution([])).toEqual([]);
+      });
+    }); 
+    */
+     describe('parameter contains all positive integer numbery strings', () => {
+      it('return a number arrray', () => {
+expect(solution(['4', '6', '3', '190'])).toEqual([4, 6, 3, 190]);
+      });
+       describe('parameter contains negative numbery strings', () => {
+      it('return a number arrray', () => {
+expect(solution(['-4', '-6', '-3', '-190'])).toEqual([-4, -6, -3, -190]);
+      });
+     });
+      describe('parameter contains positive float numbery strings', () => {
+      it('return a number arrray', () => {
+expect(solution(['0.4', '2890.6', '7.2'])).toEqual([0.4, 2890.6, 7.2]);
+      });
+     });
+      describe('parameter contains negative float numbery strings', () => {
+      it('return a number arrray', () => {
+expect(solution(['-0.4', '-2890.6', '-7.2'])).toEqual([-0.4, -2890.6, -7.2]);
+      });
+     });
+       describe('parameter contains non numbery strings', () => {
+      it('return a number arrray without NaN values', () => {
+expect(solution(['gary', '7', 'six', 'train', '65'])).toEqual([7, 65]);
+      });
+     });
+/*  
+describe('parameter contains other data types than strings', () => {
+     it('return TypeError message', () => {
+expect(solution(['pom', 703, false, undefined, ,null, -72, 3.3364])).toEqual(error1);
+      }); 
+      */
+     });
+    });
+  };
 
 // minified solution for testing your tests
 // prettier-ignore
 function secretSolution(a) { if (!Array.isArray(a)) { throw new TypeError("arrayOfStrings is not an array"); } const b = a.some(a => "string" != typeof a); if (b) { throw new TypeError("arrayOfStrings contains non-strings"); } const c = a.map(a => +a), d = c.filter(a => !Number.isNaN(a)); return d }
+
+
+
